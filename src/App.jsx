cd243e8e5a3eb409d2de562952d06aabf127a27c
SumirSingh19 <134550739+SumirSingh19@users.noreply.github.com>
2024-11-12 14:39:34 +0530

    initial files of the app

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+function App() {
+  return <div>
+    <span className="h-10 font-bold ">Hello Everyone!! I started the main project !!</span>
+  </div>;
+}
+
+export default App;
